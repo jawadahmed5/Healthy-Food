762325134697f3e329af8ccfab1bb90fb27dcad0
jawadahmed5 <jawadahmed.xwave5@gmail.com>
2024-10-19 23:08:38 +0500

    save data of add to cart in local storage

---
+++ js pages/script.js
@@ -0,0 +1,81 @@
+let totalPrice = 0;
+let totalQuantity = 0;
+
+// set data local storage 
+function loadCart() {
+
+    const cartData = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const storedTotalPrice = parseFloat(localStorage.getItem('totalPrice')) || 0;
+    const storedTotalQuantity = parseInt(localStorage.getItem('totalQuantity')) || 0;
+
+
+    totalPrice = storedTotalPrice;
+    totalQuantity = storedTotalQuantity;
+
+      document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
+    updateCartQuantity();
+
+        cartData.forEach(item => {
+    
+        addCartItem(item.imageSrc, item.name, item.price, false);
+    });
+
+      updateCartMessage();
+}
+
+
+function saveCartToLocalStorage() {
+    const cartItems = [];
+    document.querySelectorAll('#cartList .cart-item').forEach(cartItem => {
+        cartItems.push({
+            name: cartItem.querySelector('.cart-item-name').innerText,
+            price: parseFloat(cartItem.querySelector('.cart-item-price').innerText.slice(1)),
+            imageSrc: cartItem.querySelector('img').src
+        });
+    });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    localStorage.setItem('totalPrice', totalPrice.toFixed(2));
+    localStorage.setItem('totalQuantity', totalQuantity);
+}
+
+
+function isProductInCart(productName) {
+    return document.querySelector(`#cartList .cart-item[data-name="${productName}"]`) !== null;
+}
+
+
+function addCartItem(imageSrc, name, price, updateTotal = true) {
+    const cartItem = document.createElement('li');
+    cartItem.classList.add('cart-item');
+    cartItem.setAttribute('data-name', name); // To identify by product name
+    cartItem.innerHTML = `
+        <img src="${imageSrc}" alt="${name}" width="100">
+        <span class="cart-item-name fs-5">${name}</span> - <span class="cart-item-price fs-5">$${price.toFixed(2)}</span>
+        <button class="remove-from-cart bg-transparent border-0 fs-2">
+        <iconify-icon class="icon bg-white text-danger" icon="weui:delete-filled"></iconify-icon>
+        </button>
+    `;
+    document.querySelector('#cartList').appendChild(cartItem);
+
+    if (updateTotal) {
+        totalPrice += price;
+        totalQuantity += 1;
+        document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
+        updateCartQuantity(); // Update cart quantity display
+    }
+
+    cartItem.querySelector('.remove-from-cart').addEventListener('click', function() {
+        cartItem.remove();
+        totalPrice -= price;
+        totalQuantity -= 1;
+        document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
+        updateCartQuantity(); 
+        saveCartToLocalStorage();
+        updateCartMessage();
+    });
+
+    
+    saveCartToLocalStorage();
+}
+
+
